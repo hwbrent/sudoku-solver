1a8f541d311ce79f6ed4eccdbeff19da9d648645
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-02 11:45:53 +0100

    Instantiate 81 cells in Grid

--- src/Assets/Scripts/Cell.cs
+++ src/Assets/Scripts/Cell.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
+    public int row;
+    public int col;
+    public int value;
+
     // Start is called before the first frame update
     void Start()
     {
--- src/Assets/Scripts/Grid.cs
+++ src/Assets/Scripts/Grid.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+    public List<Cell> cells;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var cellPrefab = Resources.Load<Cell>("Cell");
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                var cell = Instantiate(cellPrefab);
+                cell.row = i;
+                cell.col = j;
+                cells.Add(cell);
+            }
+        }
     }
 
     // Update is called once per frame
