b04cad8289a4f1336e836a51866f88306eae6af2
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-02 11:10:46 +0100

    Instantiate a sudoku grid when game starts

--- src/Assets/Scripts/Solver.cs
+++ src/Assets/Scripts/Solver.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Solver : MonoBehaviour
 {
+
+    public GameObject grid; // Grid
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(this.grid);
     }
 
     // Update is called once per frame
