d190a8ce688b886e3bfeef919ae4eb86e920c260
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-10 19:55:24 +0100

    Remove useless code

--- src/index.js
+++ src/index.js
@@ -4,30 +4,20 @@ const Algorithms = [
     Solver
 ];
 
-/**
- * @summary Adds a `<select>` element to the page which allows the user to
- * select an algorithm with which to solve the sudoku.
- */
-function populateSelect() {
-    const select = document.querySelector('select');
-
-    // Add <option>s to <select>.
-    for (const algorithm of Algorithms) {
-        const option = document.createElement('option');
-        option.value = algorithm.name;
-        option.innerText = algorithm.name;
-        select.appendChild(option);
-    };
-}
-
+const select = document.querySelector('select');
 const startButton = document.querySelector('button');
-startButton.addEventListener('click', () => {
-    const select = document.querySelector('select');
-    const Algo = Algorithms.find(algo => algo.name === select.value);
 
+// Add <option>s to <select>.
+for (const algorithm of Algorithms) {
+    const option = document.createElement('option');
+    option.value = algorithm.name;
+    option.innerText = algorithm.name;
+    select.appendChild(option);
+};
+
+startButton.addEventListener('click', () => {
+    const Algo = Algorithms.find(algo => algo.name === select.value);
     const algoInstance = new Algo();
 });
-
-populateSelect()
 
 // startButton.click();
