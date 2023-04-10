938ab349e67b1d2d28254f962e1b5c26849a2552
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-10 13:37:03 +0100

    Add logic to get rows/cols/boxes from indices

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -7,7 +7,7 @@ export default class Grid {
 
         document.grid = this;
     }
-    
+
     init() {
         this.addToDOM();
         this.initCells();
@@ -15,7 +15,7 @@ export default class Grid {
 
     addToDOM() {
         const root = document.getElementById('root');
-        
+
         this.element = document.createElement('div');
         this.element.id = 'grid';
 
@@ -30,5 +30,35 @@ export default class Grid {
                 cell.addToDOM();
             }
         }
+    }
+
+    getRow(row_index) {
+        const startIndex = row_index * 9;
+        const endIndex = startIndex + 9;
+        return this.cells.slice(startIndex, endIndex);
+    }
+
+    getColumn(column_index) {
+        const cells = [];
+        for (let i = column_index; i < 81; i += 9) {
+            cells.push(this.cells[i]);
+        }
+        return cells;
+    }
+
+    getBox(row_index, column_index) {
+        const cells = [];
+
+        const rowLower = Math.floor(row_index / 3) * 3;
+        const colLower = Math.floor(column_index / 3) * 3;
+
+        for (let i = rowLower; i < rowLower+3; i++) {
+            for (let j = colLower; j < colLower+3; j++) {
+                console.debug(i,j);
+                cells.push(this.cells[(i*9) + j]);
+            }
+        }
+
+        return cells;
     }
 }
