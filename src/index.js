0ee4e58a82780ec9adaadd1a9ee1269ff3d3fa78
hwbrent <68196383+hwbrent@users.noreply.github.com>
2023-04-10 19:51:33 +0100

    Add <select> for selecting solving algorithm

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import Solver from './Solver/Solver.js';
 
-function start(event) {
-    event.target.remove();
+const Algorithms = [
+    Solver
+];
 
-    const solver = new Solver();
+/**
+ * @summary Adds a `<select>` element to the page which allows the user to
+ * select an algorithm with which to solve the sudoku.
+ */
+function populateSelect() {
+    const select = document.querySelector('select');
+
+    // Add <option>s to <select>.
+    for (const algorithm of Algorithms) {
+        const option = document.createElement('option');
+        option.value = algorithm.name;
+        option.innerText = algorithm.name;
+        select.appendChild(option);
+    };
 }
 
 const startButton = document.querySelector('button');
-startButton.addEventListener('click', start);
+startButton.addEventListener('click', () => {
+    const select = document.querySelector('select');
+    const Algo = Algorithms.find(algo => algo.name === select.value);
+
+    const algoInstance = new Algo();
+});
+
+populateSelect()
+
+// startButton.click();
